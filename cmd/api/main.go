d6b291a107c0bce6f21276668c329280c2c59d36
madou9 <issoufou.mamadou@gmail.com>
2025-10-27 22:08:39 +0100

    removed the http.ServerMux and used the httprouter instance as the server handler instead.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -38,7 +38,7 @@ func main() {
 	// Declarre an instance of the config struct
 	var cfg config
 
-	// Read the value of the port and env command-line flags into the config struct. We 
+	// Read the value of the port and env command-line flags into the config struct. 
 	// default to using the port number 4000 and the environment "development" if no
 	// corresponding flags are provided.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
@@ -59,18 +59,12 @@ func main() {
 		logger: logger,
 	}
 
-
-	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests 
-	// to the healthcheckHandler method (which we will create in a moment).
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	// Declare a HTTP server with some sensible timeout settings, which listens on the 
 	// port provided in the config struct and uses the servemux we created above as the 
 	// handler.
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: mux,
+		Handler: app.routes(),
 		IdleTimeout: time.Minute, // IdleTimeout is the maximum amount of time to wait for the next request when keep-alives are enabled.
 		ReadTimeout: 10 * time.Second, // ReadTimeout is the maximum duration for reading the entire request, including the body
 		WriteTimeout: 30 * time.Second, // WriteTimeout is the maximum duration before timing out writes of the response
