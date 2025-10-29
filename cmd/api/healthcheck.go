0e3f90e0bc38fcf4655191426d862c5c6c7b5255
madou9 <issoufou.mamadou@gmail.com>
2025-10-29 16:44:25 +0100

    implemented a JSON Encoding using json.Marshal() function

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
@@ -9,18 +9,32 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request){
 
-	// Create a fixed-format JSON response from a string. Notice how we're using a raw
-	// string literal (enclosed with backticks) so that we can include double-quote
-	// characters in the JSON without needing to escape them? We also use the %q verb to
-	// wrap the interpolated values in double-quotes.
-	js := `{"status" : "available", "environment" : %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-	// Set the "Content-Type: application/json" header on the response. If you forget to
-	//  this, Go will default to sending a "Content-Type: text/plain; charset=utf-8"
-	// header instead.
+	// create a map which holds the information that we want to send in the response
+	data := map[string]string{
+		"status" : "available",
+		"environment" : app.config.env,
+		"version" : version,
+	}
+
+	// pass the map to the json.Marshal() function. This returns a []byte slice 
+	// containing thee encoded JSON. If there was an error, we log it and send the client
+	// a generic error message.
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	// Append a newline to the JSON. This is just a small nicety to make it easier to 
+	// view in terminal applications.
+	js = append(js, '\n')
+
+	// At this point we know that encoding the data worked without any problems, so we 
+	// can safely set any neecessary HTTP headers for a sucessful response.
 	w.Header().Set("content-Type", "application/json")
 
-	// write the JSON as the HTTP response body.
-	w.Write([]byte(js))
+	// Use w.write() to send the []byte slice containing the JSON as the response body.
+	w.Write(js)
 
 }
