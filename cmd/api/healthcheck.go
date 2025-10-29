489162eed3dd21373b7c07293ed5003a28d0a578
madou9 <issoufou.mamadou@gmail.com>
2025-10-29 17:13:08 +0100

    simplify the code since the writeJSON() functioon is in place

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -16,25 +15,10 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version" : version,
 	}
 
-	// pass the map to the json.Marshal() function. This returns a []byte slice 
-	// containing thee encoded JSON. If there was an error, we log it and send the client
-	// a generic error message.
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
 	}
-
-	// Append a newline to the JSON. This is just a small nicety to make it easier to 
-	// view in terminal applications.
-	js = append(js, '\n')
-
-	// At this point we know that encoding the data worked without any problems, so we 
-	// can safely set any neecessary HTTP headers for a sucessful response.
-	w.Header().Set("content-Type", "application/json")
-
-	// Use w.write() to send the []byte slice containing the JSON as the response body.
-	w.Write(js)
 
 }
