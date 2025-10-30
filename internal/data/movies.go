e5febfeaf3f421e95447f91ab324c5ab2a722e38
madou9 <issoufou.mamadou@gmail.com>
2025-10-30 18:28:15 +0100

    updated the struct Movie with struct tags so that it uses snake_case for the keys.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,11 +5,11 @@ import "time"
 // Annotate the Movie struct with struct tags to control how the keys appear in the
 // JSON-encoded output.
 type Movie struct {
-	ID			int64 		`json:"id"`			// unique integer ID for movie
-	CreatedAt	time.Time	`json:"created_At"` // Timestamp for when the movie is added to our database
-	Title		string 		`json:"title"`  	// Movie title
-	Year 		int32 		`json:"year"`   	// Movie release year
-	Runtime		int32 		`json:"runtime"`	// Movie runtime (in minutes)
-	Genres		[]string 	`json:"genres"` 	// Slice of genres for the movie (romance, comedy, etc.)
-	Version		int32  		`json:"version"` 	// time the movie information is updated
+	ID			int64 		`json:"id"`					// unique integer ID for movie
+	CreatedAt	time.Time	`json:"-"` 					// Timestamp for when the movie is added to our database
+	Title		string 		`json:"title"`  			// Movie title
+	Year 		int32 		`json:"year,omitempty"`   	// Movie release year
+	Runtime		int32 		`json:"runtime,omitempty"`	// Movie runtime (in minutes)
+	Genres		[]string 	`json:"genres,omitempty"` 	// Slice of genres for the movie (romance, comedy, etc.)
+	Version		int32  		`json:"version"` 			// time the movie information is updated
 }
