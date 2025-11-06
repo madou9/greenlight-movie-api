e6fb56131cdedc4443632270108ed1eddbb2818e
madou9 <issoufou.mamadou@gmail.com>
2025-11-06 22:45:44 +0100

    replaced the errorResponse to badReuqestResponse func

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -26,7 +26,8 @@ func(app *application) createMovieHandler(w http.ResponseWriter, r *http.Request
 	// Bad Request status code, just like before.
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		// Use the new badRequestResponse() helper.
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
