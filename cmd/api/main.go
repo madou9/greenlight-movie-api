4efeb4146c8e27c9adb55b8bebb20ea30e266226
madou9 <issoufou.mamadou@gmail.com>
2025-11-09 21:57:05 +0100

    Configuring the Database Connection Pool, ‘in-use’ plus ‘idle’ connections.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -34,7 +34,10 @@ type config struct {
 	port	int
 	env		string
 	db		struct {
-		dsn		string
+		dsn				string
+		maxOpenConns	int
+		maxIdleConns	int
+		maxIdleTime		string
 	}
 }
 
@@ -66,6 +69,13 @@ func main() {
 	// Read the DSN value from the db-dsn command-line flag into the config struct. We
 	// default to using our development DSN if no flag is provided.
 	flag.StringVar(&cfg.db.dsn, "db.dsn", os.Getenv("GREENLIGHT_DB_DSN") , "PostgreSQL DSN")
+
+	// Read the connection pool settings from command-line flags into the config struct.
+	// Notice the default values that we're using?
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	// Initialize a new logger which writes messages to the standard out stream, 
@@ -120,6 +130,26 @@ func main() {
 		if err != nil {
 			return nil, err
 		}
+
+		// Set the maximum number of open (in-use + idle) connections in the pool. Note that
+		// passing a value less than or equal to 0 will mean there is no limit.
+		db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+
+		// Set the maximum number of idle connections in the pool. Again, passing a value
+		// less than or equal to 0 will mean there is no limit.
+		db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+
+		// Use the time.ParseDuration() function to convert the idle timeout duration string
+		// to a time.Duration type.
+		duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+		if err != nil {
+			return nil, err
+		}
+
+		// Set the maximum idle timeout.
+		db.SetConnMaxIdleTime(duration)
 
 		// create a context with 5-second timeout deadline.
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
