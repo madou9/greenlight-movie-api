01fbdfdce2e7a9370b65512b844c6a2cc34abdf9
madou9 <issoufou.mamadou@gmail.com>
2025-11-12 21:46:01 +0100

    renamed the verrrsion name to version to be able to make the query

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/madou9/greenlight-movie-api.git/internal/validator"
 )
 
@@ -27,7 +28,22 @@ type MovieModel struct {
 
 // Add a placeholder method for inserting a new record in the movies table.
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	// Define the SQL query for inserting a new record in
+	// the system-generated data.
+	query := `
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+	
+	// Create an args slice containing the values for the placeholders parameters from
+	// the movie struct. Declaring this slice immediately next to our SQL query helps to
+	// make it nice and clear *what values are being used where* in the query
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	// Use the QueryRow() method to execute the SQL query on our connection pool,
+	// passing in the args slice as a variadic parameter and scanning the system-
+	// generated id, created_at and version values into the movie struct.
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 // Add a placeholder method for fetching a specific record from the movies table.
