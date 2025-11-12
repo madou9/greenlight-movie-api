2991eac162b44716cd11cf86ecf87c3c1d077489
madou9 <issoufou.mamadou@gmail.com>
2025-11-12 21:44:54 +0100

    renamed the verrrsion name to version to be able to make the query

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -32,9 +32,6 @@ func(app *application) createMovieHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	// Initialize a new Validator instance
-	v := validator.New()
-
 	// Copy the values from the input struct to a new Movie struct
 	movie := &data.Movie{
 		Title: input.Title,
@@ -43,18 +40,47 @@ func(app *application) createMovieHandler(w http.ResponseWriter, r *http.Request
 		Genres: input.Genres,
 	}
 
+	// Initialize a new Validator instance
+	v := validator.New()
+
 	// Use the Valid() method to see if any of the checks failed. If they did, then use
 	// the failedValidationResponse() helper to send a response to the client, passing
 	// in the v.Errors map.
 	// Call the ValidateMovie() function and return a response containing the errors if
 	// any of the checks fail.
 	if data.ValidateMovie(v, movie); !v.Valid() {
-	app.failedValidationResponse(w, r, v.Errors)
-	return
+		app.failedValidationResponse(w, r, v.Errors)
+		return
 	}
 
-	// Dump the contents of the input struct in a HTTP response.
-	fmt.Fprintf(w, "%+v\n", input)
+	// Call the Insert() method on our movies model, passing in a pointer to the
+	// validated movie struct. This will create a record in the database and update the
+	// movie struct with the system-generated information.
+	err = app.models.Movies.Insert(movie)
+	if err != nil {
+		fmt.Println("here")
+		app.serverErrorResponse(w, r, err)
+		return 
+	}
+
+
+	// When sending a HTTP response, we want to include a Location header to let the
+	// client know which URL they can find the newly-created resource at. We make an
+	// empty http.Header map and then use the Set() method to add a new Location header,
+	// interpolating the system-generated ID for our new movie in the URL.
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+
+	// Write a JSON response with a 201 Created status code, the movie data in the
+	// response body, and the Location header.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+	// // Dump the contents of the input struct in a HTTP response.
+	// fmt.Fprintf(w, "%+v\n", input)
 }
 
 
