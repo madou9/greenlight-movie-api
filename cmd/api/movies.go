33034908f2d306cf202aba80ac6421bfe4b45087
madou9 <issoufou.mamadou@gmail.com>
2025-11-14 19:47:49 +0100

    Updated the showMovieHandler api func so that it call the GET() method.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/madou9/greenlight-movie-api.git/internal/data"
 	"github.com/madou9/greenlight-movie-api.git/internal/validator"
@@ -92,23 +92,24 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	// Create a new instance of the Movie struct, containing the ID we extracted from
-	// the URL and some dummy data. Also notice that we deliberately haven't set a 
-	// value for the Year field
+	// Call the Get() method to fetch the data for a specific movie. We also need to
+	// use the errors.Is() function to check if it returns a data.ErrRecordNotFound
+	// error, in which case we send a 404 Not Found response to the client.
 
-	movie := data.Movie{
-		ID:			id,
-		CreatedAt: 	time.Now(),
-		Title: 		"Casablanca",
-		Runtime: 	102,
-		Genres:	 	[]string{"drama", "romance", "war"},
-		Version: 	1,	
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+				app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	// Create an envelope{"movie": movie} instance and pass it to writeJSON(), instead
 	// of passing the plan movie struct
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
-
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
