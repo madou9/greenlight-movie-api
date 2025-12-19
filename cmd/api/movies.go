0dad466a3b4e14912df39d98af72bbcaa44869b3
madou9 <issoufou.mamadou@gmail.com>
2025-12-19 19:49:22 +0100

    updated the listMovie to use the new Filters struct.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -232,9 +232,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	var input struct {
 		Title		string
 		Genres		[]string
-		Page 		int
-		PageSize	int
-		Sort 		string
+		data.Filters
 	}
 
 	// Initialize a new Validator instance.
