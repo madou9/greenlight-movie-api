e47e19da6b0b5ca75f7666648c548728c661aa65
madou9 <issoufou.mamadou@gmail.com>
2025-12-22 17:11:46 +0100

    added users models

--- internal/data/models.go
+++ internal/data/models.go
@@ -16,6 +16,7 @@ var (
 
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing
@@ -23,5 +24,6 @@ type Models struct {
 func NewModel(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users: UserModel{DB: db}, // Initialize a new UserModel instance.
 	}
 }
