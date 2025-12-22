6f48e53ee54cd857af0ec9c5ec79d39b541713f9
madou9 <issoufou.mamadou@gmail.com>
2025-12-22 17:11:23 +0100

    added route for the registering user

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -31,6 +31,7 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 
 	// return the httprouter instance
 	return router
